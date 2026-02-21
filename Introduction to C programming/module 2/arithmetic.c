fcb223c043bd34ddc71754fbbdbbe75b2c82fb03
Sarowar Ciam <sarowarciam@gmail.com>
2026-02-21 21:01:34 +0600

    arithmetic operation

---
+++ Introduction to C programming/module 2/arithmetic.c
@@ -0,0 +1,33 @@
+/*#include<stdio.h>
+int main()
+{
+   int a=10,b=30,sum;
+   sum=a+b;
+   printf("%d",sum);
+   return 0;
+}*/
+
+
+/*#include<stdio.h>
+int main()
+{
+  int a=3,b=20;
+   float dvd=b*1.0/a;
+  printf("%f",dvd);
+  return 0;
+
+}*/
+
+
+
+
+#include<stdio.h>
+int main()
+{
+  int a=3,b=20,mod;
+   mod=b%a;
+  printf("%d",mod);
+  return 0;
+
+}
+
---
+++ Introduction to C programming/module 2/conditional_2.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main()
+{  
+    int tk;
+    scanf("%d",&tk);
+    if(tk>=100)
+    {
+        printf("burger khabo");
+    }
+    else if (tk>=50)
+    {
+        printf("fuchka kahbo");
+    }
+    else if(tk>=20)
+    {
+        printf("ice cream kahbo");
+    }
+    else
+    {
+        printf("khabo na kichu");
+    }
+      return 0;
+}
